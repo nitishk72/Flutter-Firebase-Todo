99aa194ea2b3db4d35f204f272dd336932b81819
NITISH KUMAR SINGH <nitishk72@gmail.com>
2018-06-08 22:36:04 +0530

    initial commit

---
+++ lib/pages/Register.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Register extends StatefulWidget {
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
